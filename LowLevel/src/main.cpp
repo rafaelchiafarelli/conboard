0541bad4333c453a5a9af3feca159479693a25c7
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2020-12-20 14:14:13 -0300

    just pushing code around

--- LowLevel/src/main.cpp
+++ LowLevel/src/main.cpp
@@ -195,34 +195,41 @@ static void sig_handler(int dummy)
 int main(int argc, char *argv[])
 {
 	stop = true;
-	static const char short_options[] = "i:p:x";
+	static const char short_options[] = "i:p:x:";
 	static const struct option long_options[] = {
 		{"ID", 1, NULL, 'i'},
 		{"port", 1, NULL, 'p'},
 		{"xml", 1, NULL, 'x'},
 		{ }
 	};
-    
+    cout<<"here 1"<<endl;
 	if(argc < 4)
-		{
-			std::cout<<"error, must specifi port and xml. Usage ./midi -p: \"hw:1,0,0\" -x \"/home/user/file.xml\""<<endl;
-			return -1;
-		}
+	{
+		std::cout<<"error, must specifi port and xml. Usage ./midi -p: \"hw:1,0,0\" -x \"/home/user/file.xml\""<<endl;
+		return -1;
+	}
     char p_name[256];
+	memset(p_name,256,0);
 	char fifoFile[256];
+	memset(fifoFile,0,256);
     string xmlFileName;
 	int c;
 	while ((c = getopt_long(argc, argv, short_options,
 		     		long_options, NULL)) != -1) {
+		cout<<strlen(optarg)<<endl;
+		cout<<optarg<<endl;
 		switch (c) {
 
 		case 'p':
-			sprintf(p_name,"%s",optarg);
+			cout<<"here 3"<<endl;
+			strcpy(p_name,optarg);
 			break;
 		case 'x':
+			cout<<"here 4"<<endl;
 			xmlFileName = string(optarg);
 			break;
-		case 'd':
+		case 'i':
+			cout<<"here 5"<<endl;
 			sprintf(fifoFile, "%s",optarg);
 			break;
 
@@ -230,8 +237,9 @@ int main(int argc, char *argv[])
 			std::cout<<"Try more information."<<endl;
 			return 1;
 		}
+		cout<<"here 6"<<endl;
 	}
-    
+    cout<<"here 2"<<endl;
     device_list();
 	/*
 		char p_name[] = {"hw:1,0,0"};
