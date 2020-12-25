b3f925ec3475effbb01ac962fa58a0da7a5c0c62
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2020-12-25 16:03:39 -0300

    pushing code around to compile

--- LowLevel/MIDI/src/midithread.cpp
+++ LowLevel/MIDI/src/midithread.cpp
@@ -130,7 +130,7 @@ void MIDI::processInput(midiSignal midiS)
     l_action.in.midi = midiS;
 
     std::set<ModeType, std::greater<ModeType>>::iterator it_mode =modes.find(l_mode);
-
+        
     std::set<Actions, std::greater<Actions>>::iterator it_act = it_mode->body_actions.find(l_action);
     
     if(it_mode != modes.end())
