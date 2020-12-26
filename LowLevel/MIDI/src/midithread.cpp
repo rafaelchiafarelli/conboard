c90922f879e6a3eddde98c5df5261d0754fd8109
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2020-12-25 23:30:36 -0300

    pushing code around to compile

--- LowLevel/MIDI/src/midithread.cpp
+++ LowLevel/MIDI/src/midithread.cpp
@@ -95,7 +95,7 @@ void MIDI::execHeader()
 void MIDI::send_keyboard(keyboardActions act)
 {
     bool hold = false;
-    char buf[1] = {'a'};
+    char buf[1] = {'letter_a'};
     cout<<"keyboard_send"<<endl;
     keyboard_send(act.type,buf,1,&hold);
 }
