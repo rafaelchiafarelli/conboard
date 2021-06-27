8f02f94f3561515c2facb1fca3770676f2ae2f80
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2021-06-27 06:21:55 -0700

    major change in the front end, and low level. a new app is inserted to handle communication between back end and lowlevel

---
+++ LowLevel/dispatcher/include/dispatcher.hpp
@@ -0,0 +1,14 @@
+#ifndef DISPATCHER_H
+#define DISPATCHER_H
+#include <iostream>
+class dispatcher{
+    private:
+        std::string fileName;
+    public:
+        dispatcher();
+        ~dispatcher();
+};
+
+
+
+#endif 
---
+++ LowLevel/dispatcher/include/main.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <atomic>
+#include <vector>
+#include <string>
+#include "midithread.hpp"
+using namespace std;
+
+
+
+
+atomic_bool stop;
+vector<raw_midi> hw_ports;
+
